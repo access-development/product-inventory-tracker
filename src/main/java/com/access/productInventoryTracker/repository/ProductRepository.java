10d3979cc8c85e5ef1cbdbaac9fa04deede812b6
James Reynolds <james.reynolds@accessdevelopment.com>
2024-07-22 12:56:07 -0600

    Update ProductRepository.java
    Remove comment about findAll() method.

--- src/main/java/com/access/productInventoryTracker/repository/ProductRepository.java
+++ src/main/java/com/access/productInventoryTracker/repository/ProductRepository.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-    // This interface method findAll() is provided by JpaRepository so no need to implement it.
     // You can add custom methods here if needed, for example: 
     // List<Product> findByCategory(String category);
 }
